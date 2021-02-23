ed6d8e9e02e4cdedba36c01388619e721892929d
bobrossrtx <bobrossrtx@gmail.com>
2021-02-23 02:53:56 +0000

    Added homepage (WIP)

---
+++ main.js
@@ -0,0 +1,6 @@
+
+var github = `
+<img style="height:auto;" alt="" class="avatar avatar-user width-full border bg-white" src="https://avatars.githubusercontent.com/u/73446766?s=460&amp;v=4" width="260" height="260">
+`
+
+Document.getElementById("github").innerHTML = github;
